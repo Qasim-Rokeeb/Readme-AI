9d41e1b0d2d753d465c1f4c53aa1e009ebc6b409
Rokeeb Qasim <qasimrokeeb@gmail.com>
2025-07-19 16:45:44 +0000

    Add dark mode to the app

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,11 +1,15 @@
 import { Logo } from '@/components/logo';
+import { ThemeToggle } from '@/components/theme-toggle';
 
 export function MainNav() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
-        <div className="flex items-center space-x-4">
+        <div className="flex-1">
           <Logo />
+        </div>
+        <div className="flex items-center space-x-4">
+          <ThemeToggle />
         </div>
       </div>
     </header>
